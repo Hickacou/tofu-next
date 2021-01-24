d6975515d7eab10532163fe9ae90f36ffc244e32
Hickacou <hickacou@gmail.com>
2021-01-24 23:38:16 +0100

    feat: accelerate reactions with 0 restTimeOffset

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ const client: BotClient = new BotClient({
 	admins: process.env.ADMINS?.split(',').filter(id => /(\d){17,19}/.test(id)),
 	owner: process.env.OWNER!,
 	prefix: process.env.GLOBAL_PREFIX!,
+	restTimeOffset: 0,
 	token: process.env.DISCORD_TOKEN!,
 	test: process.env.TEST === 'true'
 });
